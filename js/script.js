99b837d99d6748c8e08d5be06ff72dde2a530f20
loxno92 <morozovichnikita@gmail.com>
2025-01-21 01:33:00 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+const menuToggle = document.querySelector('.menu-toggle');
+const mobileNav = document.querySelector('.mobile-nav');
+
+if (menuToggle) {
+    menuToggle.addEventListener('click', () => {
+        mobileNav.classList.toggle('open');
+    });
+}
